897019ddf4c3e67251afc6d0585f923af40182c1
Manish Dange <dangemanish35@gmail.com>
2026-05-12 08:55:40 +0530

    add new problem

---
+++ RemoveDuplicatesList0083.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveDuplicatesList0083 {
+
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val);
+
+            if (temp.next != null) {
+                System.out.print(" -> ");
+            }
+
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        System.out.println("Enter sorted list elements:");
+
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode current = head;
+
+        for (int i = 1; i < n; i++) {
+            current.next = new ListNode(sc.nextInt());
+            current = current.next;
+        }
+
+        head = deleteDuplicates(head);
+
+        System.out.println("Updated Linked List:");
+        printList(head);
+
+        sc.close();
+    }
+}
