32c25cacddff007f34cfd7a3ee03b4f22c4d68c8
Manish Dange <dangemanish35@gmail.com>
2026-05-27 13:09:53 +0530

    add new problem LongestPalindrome

---
+++ LongestPalindrome0005.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class LongestPalindrome0005 {
+
+    public static String longestPalindrome(String s) {
+
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int len1 = expandFromCenter(s, i, i);
+
+            int len2 = expandFromCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+
+                start = i - (len - 1) / 2;
+
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandFromCenter(String s,
+                                       int left,
+                                       int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+
+        String s = sc.nextLine();
+
+        String result = longestPalindrome(s);
+
+        System.out.println("Longest Palindromic Substring:");
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
